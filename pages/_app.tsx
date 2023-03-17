a56f687e9150082ffb079514df026577977b2963
Ruairidh Grass <ruripg@gmail.com>
2023-03-17 16:57:12 +0000

    authentication on home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
+// import { Auth } from '@supabase/auth-ui-react'
+// import { supabase } from '@/lib/supabaseClient'
+// import { ThemeSupa } from '@supabase/auth-ui-shared'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, }: AppProps<{ initialSession: Session }>) {
+  const [supabase] = useState(() => createBrowserSupabaseClient())
+  return (
+    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+      {/* <Auth
+        supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+        providers={['google', 'facebook', 'twitter']}
+      > */}
+      <Component {...pageProps} />
+      {/* </Auth> */}
+    </SessionContextProvider>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,46 @@ import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Header from '@/components/Header'
 import Banner from '@/components/Banner'
+import { supabase } from '@/lib/supabaseClient'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
+import Account from '@/components/Account'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({ countries }: { countries: any[] }) {
+  console.log(countries);
+  const session = useSession()
+  const supabase = useSupabaseClient()
+
   return (
     <div className="max-w-7xl mx-auto bg-blue-tintiest">
-      <Head>
-        <title>Bulby Leaks</title>
-      </Head>
-      <Header />
-      <Banner />
+      {!session ? (
+        <div className='p-12'>
+          <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={['google', 'facebook', 'twitter']} />
+        </div>
+      ) : (
+        <>
+          <Head>
+            <title>Bulby Leaks</title>
+          </Head>
+          <Header />
+          <Banner />
+          <Account session={session} />
+        </>
+      )}
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  let { data } = await supabase.from('countries').select()
+
+  return {
+    props: {
+      countries: data
+    },
+  }
 }
